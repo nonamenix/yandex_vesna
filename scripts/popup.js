8df6b82cdd8c5804299c9012ab45a6da0a3949ea
nonamenix <nonamenix@gmail.com>
2014-08-15 00:35:54 +0400

    fix: скрипт запуска плагина вынесен в файл popup.js.

--- scripts/popup.js
+++ scripts/popup.js
@@ -107,6 +107,13 @@
 	};
 }( jQuery ));
 
+$(function(){
+	vesna_options = {topics:['gyroscope', 'geology'], debug: true};
+	$('.content').vesna(vesna_options);
+	$('#reload').on('click', function(){
+		$('.content').vesna(vesna_options);
+	});
+})
 
 
 
